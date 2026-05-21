4832ed90a6c2ec3c69531687168d589703f38b68
Kingsley Chinecherem Ibeh <kingsleyibeh363@gmail.com>
2026-05-21 15:15:45 +0100

    added vercel analytics

--- frontend/shoppe/app/layout.tsx
+++ frontend/shoppe/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Toaster } from "sonner";
 import { Header } from "@/app/components/header";
 import { Footer } from "@/app/components/footer";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 export const metadata: Metadata = {
